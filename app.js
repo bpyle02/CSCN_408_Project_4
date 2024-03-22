381e997995aa2497f08a9fc27c8e9c79b207f308
iownbey <54186227+iownbey@users.noreply.github.com>
2024-03-22 18:13:14 -0400

    Basic input

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 MicroModal.init();
+
+const front_textarea = document.getElementById("front");
+const back_textarea = document.getElementById("back");
+
+function submitCard() {
+    front_textarea.value = "";
+    back_textarea.value = "";
+}
